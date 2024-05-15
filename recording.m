8f2511a3fb47fe59b1c1d2b3b94bc41baac5f029
Jeremiah Apolista <122057632+japolista@users.noreply.github.com>
2024-05-15 10:12:08 -0700

    Recording

---
+++ recording.m
@@ -0,0 +1,17 @@
+function xin = recording
+%   XIN = RECORDING
+%   Output: Vector xin
+%   Input: none
+%   Description: Recording allows for the user to record words 
+Fs = 44100;
+myrecObj = audiorecorder(44100,16,1); % Fs=44.1k Hz, 16bit, mono channel
+disp('Start speaking.') % display a message ‘Start speaking’ in the command window
+recordblocking(myrecObj, 2); % record for 2 seconds
+disp('End of Recording.');
+xin = getaudiodata(myrecObj);% save the recorded signal to variable xin
+% play(xin); % play the recorded audio signal object
+play(myrecObj); % play the recorded audio signal object
+audiowrite('test.wav',xin,Fs); % create a .wav file to save the recorded signal xin.
+[y, Fs]=audioread('test.wav'); % read a .wav audio file and save to variable y.
+Sound(y,Fs); % play the signal y with correct Fs (sampling rate)
+end
