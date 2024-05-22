281b62d54475638167c5366719162cbe41bbb189
apolisj <apolisj@ET338LAB11.univ.dir.wwu.edu>
2024-05-22 11:50:12 -0700

    Created Correlation function

---
+++ words/corr.m
@@ -0,0 +1,22 @@
+function n = corr
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+Fs = 44100;
+myrecObj = audiorecorder(44100,16,1); % Fs=44.1k Hz, 16bit, mono channel
+disp('Start speaking.') % display a message ‘Start speaking’ in the command window
+recordblocking(myrecObj, 2); % record for 2 seconds
+disp('End of Recording.');
+xin = getaudiodata(myrecObj);
+cfrknorm = audioread('cfrknorm.wav');
+bucknorm = audioread('bucknorm.wav');
+jjknorm = audioread('jjknorm.wav');
+sjknorm = audioread('sjknorm.wav');
+ucknorm = audioread('ucknorm.wav');
+
+n(:, 1) = xcorr(xin, bucknorm);
+n(:, 2) = xcorr(xin, cfrknorm);
+n(:, 3) = xcorr(xin, jjknorm);
+n(:, 4)= xcorr(xin, sjknorm);
+n(:, 5) = xcorr(xin, ucknorm);
+
+end
