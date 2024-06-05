ac078e1cb621bb33d7e4e206ddf34d5290086ca1
Jeremiah Apolista <apolisj@wwu.edu>
2024-06-05 11:39:37 -0700

    finished correlation function, satya to finish word processing as function works just for karen.

--- corr.m
+++ corr.m
@@ -6,12 +6,12 @@ myrecObj = audiorecorder(44100,16,1); % Fs=44.1k Hz, 16bit, mono channel
 disp('Start speaking.') % display a message ‘Start speaking’ in the command window
 recordblocking(myrecObj, 2); % record for 2 seconds
 disp('End of Recording.');
-xin = trimWord(getaudiodata(myrecObj));
-cfrknorm = audioread('cfrknorm.wav');
-bucknorm = audioread('bucknorm.wav');
-jjknorm = audioread('jjknorm.wav');
-sjknorm = audioread('sjknorm.wav');
-ucknorm = audioread('ucknorm.wav');
+xin = getaudiodata(myrecObj);
+cfrknorm = audioread('.\normalizedWords\cfrknorm.wav');
+bucknorm = audioread('.\normalizedWords\bucknorm.wav');
+jjknorm = audioread('.\normalizedWords\jjknorm.wav');
+sjknorm = audioread('.\normalizedWords\sjknorm.wav');
+ucknorm = audioread('.\normalizedWords\ucknorm.wav');
 
 n(:, 1) = xcorr(xin, bucknorm);
 n(:, 2) = xcorr(xin, cfrknorm);
@@ -19,4 +19,17 @@ n(:, 3) = xcorr(xin, jjknorm);
 n(:, 4)= xcorr(xin, sjknorm);
 n(:, 5) = xcorr(xin, ucknorm);
 
+maxVector = [ max(n(:,1)), max(n(:,2)), max(n(:,3)), max(n(:,4)), max(n(:,5))];
+wordVector = ["Bacon Ultimate Cheeseburger", "Curly Fries", "Jumbo Jack Burger", "Sourdough Jack", "Ultimate Cheeseburger"];
+
+maxR = 0;
+position = 0;
+
+for i = 1:5
+    if maxVector(i) > maxR
+    position = i;
+    maxR = maxVector(i);
+    end
 end
+
+disp(wordVector(:, position));
