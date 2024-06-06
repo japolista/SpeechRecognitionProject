1a2bf2c11df8f360f3ad8e0c038566969c99463e
Satya Fawcett <fawcets@wwu.edu>
2024-06-06 15:11:46 -0700

    implemented trimmed words, and running on both sets of template words

--- corr.m
+++ corr.m
@@ -8,31 +8,31 @@ recordblocking(myrecObj, 2); % record for 2 seconds
 disp('End of Recording.');
 xin = getaudiodata(myrecObj);
 
-cfrknorm = audioread('.\normalizedWords\cfrknorm.wav');
-bucknorm = audioread('.\normalizedWords\bucknorm.wav');
-jjknorm = audioread('.\normalizedWords\jjknorm.wav');
-sjknorm = audioread('.\normalizedWords\sjknorm.wav');
-ucknorm = audioread('.\normalizedWords\ucknorm.wav');
+cfrknorm = audioread('.\trimmedWords\tcfrknorm.wav');
+bucknorm = audioread('.\trimmedWords\tbucknorm.wav');
+jjknorm = audioread('.\trimmedWords\tjjknorm.wav');
+sjknorm = audioread('.\trimmedWords\tsjknorm.wav');
+ucknorm = audioread('.\trimmedWords\tucknorm.wav');
 
-cfrsnorm = audioread('.\words\cfrsnorm.wav');
-bucsnorm = audioread('.\words\bucsnorm.wav');
-jjsnorm = audioread('.\words\jjsnorm.wav');
-sjsnorm = audioread('.\words\sjsnorm.wav');
-ucsnorm = audioread('.\words\ucsnorm.wav');
+cfrsnorm = audioread('.\trimmedWords\tcfrsnorm.wav');
+bucsnorm = audioread('.\trimmedWords\tbucsnorm.wav');
+jjsnorm = audioread('.\trimmedWords\tjjsnorm.wav');
+sjsnorm = audioread('.\trimmedWords\tsjsnorm.wav');
+ucsnorm = audioread('.\trimmedWords\tucsnorm.wav');
 
-cfr = (cfrknorm + cfrsnorm);
-buc = (bucknorm + bucsnorm);
-jj = (jjknorm + jjsnorm);
-sj = (sjsnorm + sjknorm);
-uc = (ucknorm + ucsnorm);
+cfr = cat(1, cfrknorm, cfrsnorm);
+buc = cat(1, bucknorm, bucsnorm);
+jj = cat(1, jjknorm, jjsnorm);
+sj = cat(1, sjsnorm, sjknorm);
+uc = cat(1, ucknorm, ucsnorm);
 
-n(:, 1) = xcorr(xin, buc);
-n(:, 2) = xcorr(xin, cfr);
-n(:, 3) = xcorr(xin, jj);
-n(:, 4)= xcorr(xin, sj);
-n(:, 5) = xcorr(xin, uc);
+corrbuc = xcorr(xin, buc);
+corrcfr = xcorr(xin, cfr);
+corrjj = xcorr(xin, jj);
+corrsj= xcorr(xin, sj);
+corruc = xcorr(xin, uc);
 
-maxVector = [ max(n(:,1)), max(n(:,2)), max(n(:,3)), max(n(:,4)), max(n(:,5))];
+maxVector = [ max(corrbuc), max(corrcfr), max(corrjj), max(corrsj), max(corruc)];
 wordVector = ["Bacon Ultimate Cheeseburger", "Curly Fries", "Jumbo Jack Burger", "Sourdough Jack", "Ultimate Cheeseburger"];
 
 maxR = 0;
@@ -45,4 +45,4 @@ for i = 1:5
     end
 end
 
-disp(wordVector(:, position));
+n = wordVector(:, position);
